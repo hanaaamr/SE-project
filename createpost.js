e49cf11d8bfac251b787745eeec5e6675dd53211
hanaaamr <161610366+hanaaamr@users.noreply.github.com>
2024-05-12 02:08:51 +0300

    submit

--- createpost.js
+++ createpost.js
@@ -1,8 +1,4 @@
 function submitted() {
     alert('Your Post is Submitted!');
-    redirectToPage( "organization.html");
 }
 
-function redirectToPage(pageUrl) {
-    window.location.href = pageUrl;
-}
